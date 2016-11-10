d843b3688f73bf47eda11a13473e09f26d7e7405
oppiheht <oppiheht@gmail.com>
2016-11-09 17:15:46 -0800

    Added back button to scores screen.

--- core/src/com/blue/gdx/slide/screen/ScoresScreen.java
+++ core/src/com/blue/gdx/slide/screen/ScoresScreen.java
@@ -5,13 +5,17 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ActorGestureListener;
 import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.blue.gdx.slide.SlideGame;
 import com.blue.gdx.slide.actor.Background;
 import com.blue.gdx.slide.actor.StatusText;
+import com.blue.gdx.slide.util.ButtonFactory;
 import com.blue.gdx.slide.util.HighScoreKeeper;
 
 public class ScoresScreen extends ScreenAdapter {
@@ -30,11 +34,12 @@ public class ScoresScreen extends ScreenAdapter {
 
       StatusText titleText = createTitleText();
       StatusText scoreText = createScoreText();
+      ImageButton backButton = createBackButton();
 
       stage.addActor(new Background(game.getAssetManager()));
       stage.addActor(titleText);
       stage.addActor(scoreText);
-
+      stage.addActor(backButton);
    }
 
    private StatusText createTitleText() {
@@ -57,6 +62,17 @@ public class ScoresScreen extends ScreenAdapter {
       return text;
    }
 
+   private ImageButton createBackButton() {
+      ImageButton backButton = ButtonFactory.createBackButton(game.getAssetManager(), new ActorGestureListener() {
+         @Override
+         public void tap(InputEvent event, float x, float y, int count, int button) {
+            super.tap(event, x, y, count, button);
+            backToHome();
+         }
+      });
+      return backButton;
+   }
+
    @Override
    public void render(float delta) {
       Gdx.gl.glClearColor(0, 0, 0, 0);
@@ -67,10 +83,14 @@ public class ScoresScreen extends ScreenAdapter {
       queryBackKeyPressed();
    }
 
+   private void backToHome() {
+      game.setScreen(new StartScreen(game));
+      dispose();
+   }
+
    protected void queryBackKeyPressed() {
       if (Gdx.input.isKeyJustPressed(Input.Keys.BACK)) {
-         game.setScreen(new StartScreen(game));
-         dispose();
+         backToHome();
       }
    }
 }
