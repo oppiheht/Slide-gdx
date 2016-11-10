f84ade5c690f3d617bc24f0cf9a314ca428d3ce1
oppiheht <oppiheht@gmail.com>
2016-11-09 17:15:35 -0800

    Start screen buttons now use ButtonFactory to common up some code.

--- core/src/com/blue/gdx/slide/screen/StartScreen.java
+++ core/src/com/blue/gdx/slide/screen/StartScreen.java
@@ -5,14 +5,12 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ActorGestureListener;
-import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.blue.gdx.slide.SlideGame;
@@ -20,20 +18,14 @@ import com.blue.gdx.slide.actor.Background;
 import com.blue.gdx.slide.screen.game.InfiniteGameScreen;
 import com.blue.gdx.slide.screen.game.PerfectionistGameScreen;
 import com.blue.gdx.slide.screen.game.TimedGameScreen;
+import com.blue.gdx.slide.util.ButtonFactory;
 import com.blue.gdx.slide.util.SlideAssetManager;
 
 public class StartScreen extends ScreenAdapter {
    
    private final SlideGame game;
-   
    private Stage stage;
    
-   private Texture gameLogoTexture;
-   private ImageButton playTimedButton;
-   private ImageButton playPerfectionistButton;
-   private ImageButton playInfiniteButton;
-   private ImageButton scoresButton;
-
    public StartScreen(SlideGame game) {
       this.game = game;
    }
@@ -45,15 +37,15 @@ public class StartScreen extends ScreenAdapter {
       
       Background background = new Background(game.getAssetManager());
 
-      gameLogoTexture = game.getAssetManager().get(SlideAssetManager.LOGO, Texture.class);
+      Texture gameLogoTexture = game.getAssetManager().get(SlideAssetManager.LOGO, Texture.class);
       Image logo = new Image(gameLogoTexture);
       logo.setSize(Gdx.graphics.getWidth() * 0.8f, Gdx.graphics.getHeight() * 0.2f);
       logo.setPosition(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() * 0.77f, Align.center);
       
-      createPlayTimedButton();
-      createPlayPerfectionistButton();
-      createPlayInfiniteButton();
-      createScoresButton();
+      ImageButton playTimedButton = createPlayTimedButton();
+      ImageButton playPerfectionistButton = createPlayPerfectionistButton();
+      ImageButton playInfiniteButton = createPlayInfiniteButton();
+      ImageButton scoresButton = createScoresButton();
             
       stage.addActor(background);
       background.addAction(Actions.sequence(Actions.alpha(0), Actions.fadeIn(3)));
@@ -69,80 +61,68 @@ public class StartScreen extends ScreenAdapter {
       scoresButton.addAction(Actions.sequence(Actions.alpha(0), Actions.fadeIn(2)));
    }
 
-   private void createPlayTimedButton() {
-      playTimedButton = new ImageButton(
-            new TextureRegionDrawable(
-                  new TextureRegion(game.getAssetManager().get(SlideAssetManager.PLAYTIMEDBUTTON, Texture.class))),
-            new TextureRegionDrawable(
-                  new TextureRegion(game.getAssetManager().get(SlideAssetManager.PLAYTIMEDBUTTON, Texture.class)))
-            );
-      playTimedButton.setWidth(Gdx.graphics.getWidth() * 0.7f);
-      playTimedButton.setPosition(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() * 0.5f, Align.center);
-      playTimedButton.addListener(new ActorGestureListener() {
-         @Override
-         public void tap(InputEvent event, float x, float y, int count, int button) {
-            super.tap(event, x, y, count, button);
-            game.setScreen(new TimedGameScreen(game));
-            dispose();
-         }
-      });
+   private ImageButton createPlayTimedButton() {
+      ImageButton playTimedButton = ButtonFactory.createStartScreenButton(
+            game.getAssetManager().get(SlideAssetManager.PLAYTIMEDBUTTON, Texture.class),
+            Gdx.graphics.getHeight() * 0.5f,
+            new ActorGestureListener() {
+               @Override
+               public void tap(InputEvent event, float x, float y, int count, int button) {
+                  super.tap(event, x, y, count, button);
+                  game.setScreen(new TimedGameScreen(game));
+                  dispose();
+               }
+            }
+      );
+      return playTimedButton;
    }
    
-   private void createPlayPerfectionistButton() {
-      playPerfectionistButton = new ImageButton(
-            new TextureRegionDrawable(
-                  new TextureRegion(game.getAssetManager().get(SlideAssetManager.PLAYPERFECTIONISTBUTTON, Texture.class))),
-            new TextureRegionDrawable(
-                  new TextureRegion(game.getAssetManager().get(SlideAssetManager.PLAYPERFECTIONISTBUTTON, Texture.class)))
-            );
-      playPerfectionistButton.setWidth(Gdx.graphics.getWidth() * 0.7f);
-      playPerfectionistButton.setPosition(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() * 0.38f, Align.center);
-      playPerfectionistButton.addListener(new ActorGestureListener() {
-         @Override
-         public void tap(InputEvent event, float x, float y, int count, int button) {
-            super.tap(event, x, y, count, button);
-            game.setScreen(new PerfectionistGameScreen(game));
-            dispose();
-         }
-      });
+   private ImageButton createPlayPerfectionistButton() {
+      ImageButton playPerfectionistButton = ButtonFactory.createStartScreenButton(
+            game.getAssetManager().get(SlideAssetManager.PLAYPERFECTIONISTBUTTON, Texture.class),
+            Gdx.graphics.getHeight() * 0.38f,
+            new ActorGestureListener() {
+               @Override
+               public void tap(InputEvent event, float x, float y, int count, int button) {
+                  super.tap(event, x, y, count, button);
+                  game.setScreen(new PerfectionistGameScreen(game));
+                  dispose();
+               }
+            }
+      );
+      return playPerfectionistButton;
    }
    
-   private void createPlayInfiniteButton() {
-      playInfiniteButton = new ImageButton(
-            new TextureRegionDrawable(
-                  new TextureRegion(game.getAssetManager().get(SlideAssetManager.PLAYINFINITEBUTTON, Texture.class))),
-            new TextureRegionDrawable(
-                  new TextureRegion(game.getAssetManager().get(SlideAssetManager.PLAYINFINITEBUTTON, Texture.class)))
-            );
-      playInfiniteButton.setWidth(Gdx.graphics.getWidth() * 0.7f);
-      playInfiniteButton.setPosition(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() * 0.26f, Align.center);
-      playInfiniteButton.addListener(new ActorGestureListener() {
-         @Override
-         public void tap(InputEvent event, float x, float y, int count, int button) {
-            super.tap(event, x, y, count, button);
-            game.setScreen(new InfiniteGameScreen(game));
-            dispose();
-         }
-      });
+   private ImageButton createPlayInfiniteButton() {
+      ImageButton playInfiniteButton = ButtonFactory.createStartScreenButton(
+            game.getAssetManager().get(SlideAssetManager.PLAYINFINITEBUTTON, Texture.class),
+            Gdx.graphics.getHeight() * 0.26f,
+            new ActorGestureListener() {
+               @Override
+               public void tap(InputEvent event, float x, float y, int count, int button) {
+                  super.tap(event, x, y, count, button);
+                  game.setScreen(new InfiniteGameScreen(game));
+                  dispose();
+               }
+            }
+      );
+      return playInfiniteButton;
    }
 
-   private void createScoresButton() {
-      scoresButton = new ImageButton(
-            new TextureRegionDrawable(
-                  new TextureRegion(game.getAssetManager().get(SlideAssetManager.SCORESBUTTON, Texture.class))),
-            new TextureRegionDrawable(
-                  new TextureRegion(game.getAssetManager().get(SlideAssetManager.SCORESBUTTON, Texture.class)))
+   private ImageButton createScoresButton() {
+      ImageButton scoresButton = ButtonFactory.createStartScreenButton(
+            game.getAssetManager().get(SlideAssetManager.SCORESBUTTON, Texture.class),
+            Gdx.graphics.getHeight() * 0.14f,
+            new ActorGestureListener() {
+               @Override
+               public void tap(InputEvent event, float x, float y, int count, int button) {
+                  super.tap(event, x, y, count, button);
+                  game.setScreen(new ScoresScreen(game));
+                  dispose();
+               }
+            }
       );
-      scoresButton.setWidth(Gdx.graphics.getWidth() * 0.7f);
-      scoresButton.setPosition(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() * 0.14f, Align.center);
-      scoresButton.addListener(new ActorGestureListener() {
-         @Override
-         public void tap(InputEvent event, float x, float y, int count, int button) {
-            super.tap(event, x, y, count, button);
-            game.setScreen(new ScoresScreen(game));
-            dispose();
-         }
-      });
+      return scoresButton;
    }
 
    @Override
--- core/src/com/blue/gdx/slide/util/ButtonFactory.java
+++ core/src/com/blue/gdx/slide/util/ButtonFactory.java
@@ -5,13 +5,13 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ActorGestureListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Align;
 
 public class ButtonFactory {
+
    public static ImageButton createBackButton(AssetManager assetManager, ActorGestureListener listener) {
       ImageButton backButton = new ImageButton(
             new TextureRegionDrawable(
@@ -23,5 +23,18 @@ public class ButtonFactory {
       backButton.setPosition(Gdx.graphics.getWidth() / 5, Gdx.graphics.getHeight() * 0.05f, Align.center);
       backButton.addListener(listener);
       return backButton;
+   }
+
+   public static ImageButton createStartScreenButton(Texture texture, float height, ActorGestureListener listener) {
+      ImageButton imageButton = new ImageButton(
+            new TextureRegionDrawable(
+                  new TextureRegion(texture)),
+            new TextureRegionDrawable(
+                  new TextureRegion(texture))
+      );
+      imageButton.setWidth(Gdx.graphics.getWidth() * 0.7f);
+      imageButton.setPosition(Gdx.graphics.getWidth() / 2, height, Align.center);
+      imageButton.addListener(listener);
+      return imageButton;
    }
 }
