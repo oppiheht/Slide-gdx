a26b932373d98fd705717b558b7c964e72299bf4
oppiheht <oppiheht@gmail.com>
2016-11-13 15:55:30 -0800

    Cleaned up duplicated '/' character in asset location string

--- core/src/com/blue/gdx/slide/util/SlideAssetManager.java
+++ core/src/com/blue/gdx/slide/util/SlideAssetManager.java
@@ -6,28 +6,28 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 
 public class SlideAssetManager extends AssetManager {
 
-   private static String assetLocation = "nikky_space";
+   private static String assetLocation = "nikky_space/";
    
    public static String FONTFILE = "fonts/colo_pro_black.fnt";
    public static String FONTTEXTURE = "fonts/colo_pro_black.png";
-   public static String BACKGROUND = assetLocation+"/menuBg.png";
+   public static String BACKGROUND =  assetLocation+"menuBg.png";
    
-   public static String LOGO = assetLocation+"/logo.png";
-   public static String PLAYTIMEDBUTTON = assetLocation+"/playTimedButton.png";
-   public static String PLAYINFINITEBUTTON = assetLocation+"/playInfiniteButton.png";
-   public static String PLAYPERFECTIONISTBUTTON = assetLocation+"/playPerfectionistButton.png";
-   public static String SCORESBUTTON = assetLocation+"/scoresButton.png";
+   public static String LOGO =  assetLocation+"logo.png";
+   public static String PLAYTIMEDBUTTON =  assetLocation+"playTimedButton.png";
+   public static String PLAYINFINITEBUTTON =  assetLocation+"playInfiniteButton.png";
+   public static String PLAYPERFECTIONISTBUTTON =  assetLocation+"playPerfectionistButton.png";
+   public static String SCORESBUTTON =  assetLocation+"scoresButton.png";
 
-   public static String BACKBUTTON = assetLocation+"/backButton.png";
-   public static String RESETBUTTON = assetLocation+"/resetButton.png";
+   public static String BACKBUTTON =  assetLocation+"backButton.png";
+   public static String RESETBUTTON =  assetLocation+"resetButton.png";
 
    public static final String[] ROCKS = {
-           assetLocation+"/asteroid1.png",
-           assetLocation+"/asteroid2.png",
-           assetLocation+"/asteroid3.png"
+            assetLocation+"asteroid1.png",
+            assetLocation+"asteroid2.png",
+            assetLocation+"asteroid3.png"
    };
-   public static String PLAYER = assetLocation+"/player.png";
-   public static String GOAL = assetLocation+"/goal.png";
+   public static String PLAYER =  assetLocation+"player.png";
+   public static String GOAL =  assetLocation+"goal.png";
                  
    
    public void loadAllAssetsBlocking() {
